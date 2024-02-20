0dc4fedf03834dd140bb77b28d23cfb45e3b306c
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2024-02-20 01:57:56 +0100

    2.58

--- src/components/shared/TransformationForm.tsx
+++ src/components/shared/TransformationForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useTransition } from "react";
 
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -22,9 +22,12 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants";
+import { aspectRatioOptions, creditFee, defaultValues, transformationTypes } from "@/constants";
 import { CustomField } from "./CustomField";
-import { AspectRatioKey } from "@/lib/utils";
+import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
+import { updateCredits } from "@/lib/actions/user.actions";
+import MediaUploader from "./MediaUploader";
+import TransformedImage from "./TransformedImage";
 export const formSchema = z.object({
   title: z.string(),
   aspectRatio: z.string().optional(),
@@ -46,8 +49,11 @@ const TransformationForm = ({
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isTransforming, setIsTransforming] = useState(false);
   const [transformationConfig, setTransformationConfig] = useState(config);
+  
   const [newTransformation, setnewTransformation] =
     useState<Transformations | null>(null);
+
+    const [isPending, startTransition] = useTransition()
   const initialValues =
     data && action === "Update"
       ? {
@@ -68,11 +74,43 @@ const TransformationForm = ({
     console.log(values);
   }
 
-  const onSelectFieldHandler = (value:string, onChangeField:(value: string) => void) => {}
+  const onSelectFieldHandler = (value:string, onChangeField:(value: string) => void) => {
+    const imageSize = aspectRatioOptions[value as AspectRatioKey];
+    setImage((prevState:any) => ({
+        ...prevState,
+        aspectRation:imageSize.aspectRatio,
+        width:imageSize.width,
+        height:imageSize.height
+    }))
+    setnewTransformation(transformationType.config);
+    return onChangeField(value);
+  }
 
-  const onTransformHandler = () => {}
+  const onTransformHandler = async () => {
+    setIsTransforming(true);
+    setTransformationConfig(
+        deepMergeObjects(newTransformation,
+            transformationConfig)
+    )
+    setnewTransformation(null);
 
-  const onInputChangeHandler = (fieldName:string, value: string, type:string, onChangeField:(value: string) => void) => {}
+    startTransition(async () => {
+        // await updateCredits(userId, creditFee)
+    })
+  }
+
+  const onInputChangeHandler = (fieldName:string, value: string, type:string, onChangeField:(value: string) => void) => {
+    debounce(() => {
+        setnewTransformation((prevState:any) => ({
+            ...prevState,
+            [type] :{
+                ...prevState?.[type],
+                [fieldName === 'prompt' ? 'prompt' : 'to']:value
+            }
+        }))
+        return onChangeField(value);
+    }, 1000);
+  }
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -81,7 +119,7 @@ const TransformationForm = ({
           name="title"
           formLabel="Image Title"
           className="w-full"
-          render={({ field }) => <Input {...field} className="inout-field" />}
+          render={({ field }) => <Input {...field} className="input-field" />}
         />
 
         {type === "fill" && (
@@ -148,6 +186,30 @@ const TransformationForm = ({
                 )}
             </div>
         )}
+        <div className='media-uploader-field'>
+            <CustomField 
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({field}) => (
+                <MediaUploader 
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+                />
+            )}
+            />
+            <TransformedImage 
+            image={image}
+            type={type}
+            title={form.getValues().title}
+            isTransforming={isTransforming}
+            setIsTransforming={setIsTransforming}
+            transformationConfig={transformationConfig}
+            />
+        </div>
         <div className="flex flex-col gap-4">
         <Button 
         className="submit-button capitalize"
